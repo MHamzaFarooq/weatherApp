ca0b0e4c6bade46011fffd3c40527d1ac0374944
Hamza <hamzafarooq49ml@gmail.com>
2026-06-03 14:48:53 +0500

    Fixed the build for deployment

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,20 @@
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import type { Coords } from "../types";
 import { useEffect } from "react";
+
+// Fix Leaflet's broken marker icons in Vite/prod builds
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 type Props = {
   coords: Coords;
   onMapClick: (lat: number, lon: number) => void;
